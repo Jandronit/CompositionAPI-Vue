e8eeaa87e216880fd1c9cf1336928799bc98d599
Jandronit <jandrohen@gmail.com>
2023-04-16 20:51:22 +0200

    (lesson 226-226 course vue) Counter using composition API

--- src/views/Counter.vue
+++ src/views/Counter.vue
@@ -1,13 +1,33 @@
+<script>
+import {defineComponent, ref} from "vue";
+export default defineComponent( {
+    name: "Counter",
+    props: {},
+    emits: [],
+    setup() {
+        const counter = ref(0);
+        // const increase = () => {
+        //     counter.value++;
+        // };
+        // const decrease = () => {
+        //     counter.value--;
+        // };
+        return {
+            counter,
+            increase: () => counter.value++, // increase,
+            decrease: () => counter.value--, // decrease,
+        };
+    },
+});
+</script>
 <template>
-  <h1>Counter View</h1>
+    <h1>Counter View</h1>
+    <h2>{{counter}}</h2>
+    <button @click="decrease">-1</button>
+    <button @click="increase">+1</button>
 </template>
 
-<script>
-export default {
-    name: "Counter"
-}
-</script>
 
-<style scoped>
+<style>
 
 </style>
