3ad7695b2f20c97df2b545382046e7e311383152
Jandronit <jandrohen@gmail.com>
2023-04-16 21:19:42 +0200

    (lesson 229 course vue) Ref vs Reactive

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,12 +1,49 @@
-<template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-  </div>
-</template>
-
 <script>
+import {defineComponent, ref, reactive} from 'vue';
+export default defineComponent({
+  name: 'HomeView',
+  setup() {
+    const ironman = ref({name: 'Tony Stark', age: 45});
+    const hulk = reactive({name: 'Bruce Banner', age: 45});
 
-export default {
-  name: 'HomeView'
-}
+    const ironmanIsAlive = reactive(false);
+    const hulkIsAlive = ref(true);
+
+    const changeStatus = () => {
+        hulkIsAlive.value = !hulkIsAlive.value;
+        // not working
+        // ironmanIsAlive = !ironmanIsAlive;
+    }
+      return {
+        ironman,
+        hulk,
+        ironmanIsAlive,
+        hulkIsAlive,
+        changeStatus,
+        changeIronman: () => {
+            ironman.value.name = 'Ironman';
+            ironman.value.age = 50;
+        },
+        changeHulk: () => {
+            hulk.name = 'Hulk';
+            hulk.age = 50;
+        }
+      };
+  },
+})
 </script>
+<template>
+    <div class="home">
+        <h1>Reactive vs Ref</h1>
+        <h3>{{ironman}}</h3>
+        <h3>{{hulk}}</h3>
+
+        <button @click="changeIronman">Change Ironman</button>
+        <button @click="changeHulk">Change Hulk</button>
+
+        <h3>Ironman:{{ironmanIsAlive}}</h3>
+        <h3>Hulk:{{hulkIsAlive}}</h3>
+
+        <button @click="changeStatus">Change Hulk Status</button>
+    </div>
+</template>
