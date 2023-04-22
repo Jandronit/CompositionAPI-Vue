9d4b0abae452700c8731884659a291c06a1b77c8
Jandronit <jandrohen@gmail.com>
2023-04-22 10:45:47 +0200

    (lesson 247 course vue) Design of todo list

--- src/views/TodoVuex.vue
+++ src/views/TodoVuex.vue
@@ -2,8 +2,19 @@
 <h1>Thanos Todo List</h1>
   <h4>Pending: {{ pending.length }}</h4>
   <hr>
-  <h4>Todos: {{ allTodos.length }}</h4>
-  <h4>Completed: {{ completed.length }}</h4>
+  <button class="active">All</button>
+  <button>Pending</button>
+  <button>Completed</button>
+
+  <div>
+    <ul>
+      <li v-for="todo in allTodos" :key="todo.id"
+          :class="{'completed': todo.completed}">
+        <input type="checkbox" v-model="todo.completed">
+        {{ todo.text }}
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script>
@@ -23,5 +34,36 @@ export default defineComponent({
 </script>
 
 <style scoped>
-
+div {
+  display: flex;
+  justify-content: center;
+  text-align: center;
+  border: 1px solid #ccc;
+  padding: 10px;
+  margin: 10px;
+}
+ul {
+  width: 18.75rem;
+  list-style: none;
+  padding: 0;
+  text-align: left;
+}
+li {
+  margin: 0.5rem 0;
+  cursor: pointer;
+}
+.active {
+    background-color: #2c3e50;
+    color: white;
+}
+.completed {
+    text-decoration: line-through;
+}
+button {
+  border: none;
+  padding: 0.625rem 1.25rem;
+  margin: 0.625rem;
+  cursor: pointer;
+  border-radius: 0.2rem;
+}
 </style>
