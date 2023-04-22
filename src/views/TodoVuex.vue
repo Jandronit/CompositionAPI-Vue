bbdb02a26dde5488f3c92689020d9e892080490f
Jandronit <jandrohen@gmail.com>
2023-04-22 10:03:14 +0200

    (lesson 244 course vue) State design

---
+++ src/views/TodoVuex.vue
@@ -0,0 +1,15 @@
+<template>
+<h1>Thanos Todo List</h1>
+</template>
+
+<script>
+import {defineComponent} from 'vue';
+
+export default defineComponent({
+    name: "TodoVuex"
+})
+</script>
+
+<style scoped>
+
+</style>
